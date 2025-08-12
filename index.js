5a6de80f933398ecd665ed3dfe7f498604d69f82
lovegupta112 <loveguptaofficials@gmail.com>
2025-08-13 02:50:20 +0530

    implemented socket for appointement and chat

--- index.js
+++ index.js
@@ -1,7 +1,74 @@
-const app= require("express")();
-const PORT=3000;
+const { WebSocketServer } = require("ws");
+const PORT = 8080;
+const wss = new WebSocketServer({ port: PORT });
 
+let users = new Map();
+let rooms = new Map();
 
+wss.on("connection", (socket) => {
+  console.log(`server is listening on port: ${PORT}`);
 
-app.listen(PORT,()=>console.log(`server is listening on port: ${PORT}`));
+  try {
+    socket.on("message", (message) => {
+      console.log("Message: ", JSON.parse(message));
+      let data = Object.keys(message)?.length > 0 ? JSON.parse(message) : {};
 
+      if (!data.userId || !data.therapistId) {
+        return; //TODO do proper manage of error
+      }
+
+      if (data?.type == "appoinment") {
+        if (!users.has(data?.userId)) {
+          users.set(data.userId, socket);
+        }
+        if (!users.has(data?.therapistId)) {
+          users.set(data.therapistId, socket);
+        }
+
+        let therapistSocket = users.get(data.therapistId);
+        const response = {
+          message: data?.message || "appoinment_request",
+          userId: data.userId,
+          time: data.time,
+          therapistId: data.therapistId,
+        };
+        therapistSocket.send(JSON.stringify(response));
+      } else if (data?.type == "appoinment_fixed") {
+        if (!data.roomId) {
+          return; //TODO do proper manage of error
+        }
+        if (!rooms.has(data?.roomId)) {
+          rooms.set(data.roomId, [data?.userId, data?.therapistId]);
+        }
+      } else if (data?.type == "chat") {
+        if (!data.roomId) {
+          return; //TODO do proper manage of error
+        }
+        let roomIdMembersInfo = rooms.get(data.roomId) || [];
+        let receiverId = roomIdMembersInfo.find(
+          (memId) => users.get(memId) != socket
+        );
+        let receiverSocket = users.get(receiverId);
+
+        if (!receiverSocket) {
+          return;
+        }
+
+        let resp = {
+          message: data?.message,
+          userId: data.userId,
+          time: data.time,
+          therapistId: data.therapistId,
+        };
+
+        receiverSocket.send(JSON.stringify(resp));
+      }
+    });
+  } catch (error) {
+    console.error("Message handling error:", err);
+  }
+  socket.on("close", () => {
+    console.log("Connection closed");
+  });
+  socket.on("error", console.error);
+});
